c1dec1acf3364a67ae983df588baec478d94a09d
veraziqa <veraziqa72666@gmail.com>
2025-01-14 22:56:43 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-
-// Placeholder for any future JavaScript animations or interactions
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Document is fully loaded and parsed.');
-    // Add any JavaScript animations or interactions here
-});
